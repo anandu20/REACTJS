bd814f019ecec0b099a0bd417cc5ba7eac233c23
anandu20 <ananduramachandran411@gmail.com>
2024-12-05 09:14:42 +0530

    addded

---
+++ tailwind/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+  <div className="border border-green-500 ... border-4 border-x-4" >
+  <div>01</div>
+</div>
+
+
+    
+  )
+}
+
+export default App
